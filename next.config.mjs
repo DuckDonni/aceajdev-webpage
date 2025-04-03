a5845afa178419546cfcc636cf251e9a3b854d6d
DuckDonni <jonahsmith2010@gmail.com>
2025-04-03 00:28:32 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,11 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable default image optimization
+    unoptimized: true,
   },
-  assetPrefix: isProd ? '/aceajdev-webpage/' : '',
-  //basePath: isProd ? '/DuckDonni' : '',
-  basePath: isProd ? '/aceajdev-webpage' : '',
+  // Remove or set these to empty strings if using a custom domain at the root
+  assetPrefix: isProd ? '' : '',
+  basePath: isProd ? '' : '',
   output: 'export',
   distDir: 'dist',
 };
